459d31a844b6475b435cebd30be6bf755dc3f724
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2023-02-15 13:55:51 +0000

    .

---
+++ product-preview-card-component/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='card'>
+      <img src='./images/image-product-desktop.jpg'></img>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+Preview
+
+Gabrielle Essence Eau De Parfum
+
+A floral, solar and voluptuous interpretation composed by Olivier Polge,
+Perfumer-Creator for the House of CHANEL.
+
+$149.99
+$169.99
+
+Add to Cart
+*/
